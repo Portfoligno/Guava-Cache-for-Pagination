ce3eb60b4716a20c0c39eb8692f90e02e4ff7392
Nicholas Yip <yiplunkwun@gmail.com>
2018-07-07 22:46:14 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  `java-library`
+  kotlin("jvm") version "1.2.51"
+}
+tasks.getByName<Wrapper>("wrapper") {
+  gradleVersion = "4.8.1"
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_7
+  targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+repositories {
+  jcenter()
+}
+dependencies {
+  implementation(kotlin("stdlib"))
+  api("com.google.guava:guava:20.0")
+}
